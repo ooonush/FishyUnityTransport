5c8930280c07ded11aa6e68c9f85de4de660d0d4
Alven <ooonush@gmail.com>
2024-04-05 11:30:34 +0500

    Added LocalConnectionStateExtensions.cs

---
+++ Assets/FishNet/Plugins/FishyUnityTransport/LocalConnectionStateExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace FishNet.Transporting.UTP
+{
+    internal static class LocalConnectionStateExtensions
+    {
+        public static bool IsStartingOrStarted(this LocalConnectionState state)
+        {
+            return state == LocalConnectionState.Starting || state == LocalConnectionState.Started;
+        }
+        
+        public static bool IsStoppingOrStopped(this LocalConnectionState state)
+        {
+            return state == LocalConnectionState.Stopping || state == LocalConnectionState.Stopped;
+        }
+    }
+}
